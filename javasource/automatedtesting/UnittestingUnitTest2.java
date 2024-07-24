44f7420a123a5faa3ae6ada72754708ac8db474c
Scott Perkins <whatisbyandby@gmail.com>
2024-07-24 17:28:18 +0000

    Initial app upload.

---
+++ javasource/automatedtesting/UnittestingUnitTest2.java
@@ -0,0 +1,35 @@
+package automatedtesting;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import automatedtesting.AbstractUnitTest;
+
+import static org.junit.Assert.assertTrue;
+
+public class UnittestingUnitTest2 extends AbstractUnitTest {
+
+	@Before
+	public void setup() throws InterruptedException {
+		Thread.sleep(500);
+	}
+
+	@After
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(500);
+	}
+
+	@Test
+	public void evenMoreUnitTests() throws InterruptedException {
+		this.startTimeMeasure();
+
+		this.reportStep(
+				"By inheriting from AbstractUnitTest some utility methods are provided and time can be tracted in a more reliable way (without counting setup and teardown)");
+		Thread.sleep(1000);
+
+		assertTrue(true);
+
+		this.endTimeMeasure();
+	}
+
+}
